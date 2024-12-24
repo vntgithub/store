28985a0375bd3f57829e1d63939b4c55d1ab3e57
vntgihub <vntrieu25879@gmail.com>
2024-12-24 15:47:46 +0700

    CRUD products

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Store
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Set the default locale to :en (or your desired locale)
+    config.i18n.default_locale = :en
   end
 end
